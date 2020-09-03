0605fbeedccdcb08e95aa21a00e7c87d82bef585
Никита Пермяков <permikov134@mail.ru>
2020-09-04 01:18:53 +0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-app';
+
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+
 
 function App() {
   return (
-    <div className="app">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3">
-            <div className="app__sidebar">
+    <Router>
+      <div className="app">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
               <Sidebar />
             </div>
-          </div>
-          <div className="col-lg-9 app__main-content">
-            <Navbar />
+            <div className="col-lg-9 app__main-content">
+              <Navbar />
+              <Switch>
+                <Route exact path="/">
+                  <About />
+                </Route>
+                <Route path="/resume">
+                  <Resume />
+                </Route>
+                <Route path="/projects" component={Projects}/>
+                <Route>
+                  <Redirect to="/" />
+                </Route>
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Navbar = () => {
-    const [active, setActive] = useState('About')
+    const [active, setActive] = useState('')
+
+    useEffect(() => {
+        let currentUrl = window.location.href
+        if(currentUrl.endsWith('/'))
+            setActive('About');
+        else if (currentUrl.endsWith('/projets'))
+            setActive('Projects');
+        else if (currentUrl.endsWith('/resume'))
+            setActive('Resume');
+    }, [active])
 
     return (
         <div className="navbar">
@@ -11,13 +22,19 @@ const Navbar = () => {
             </div>
             <div className="navbar__items">
             {active !== 'About' &&
+                <Link to="/">
                 <div className="navbar__item" onClick={() => setActive('About')}>About</div>
+                </Link>
             }{
                 active !== 'Resume' ?
-                <div className="navbar__item" onClick={() => setActive('Resume')}>Resume</div>
-                : null
-            }{active !== 'Projects' &&
-                <div className="navbar__item" onClick={() => setActive('Projects')}>Projects</div>
+                <Link to="/resume">
+                    <div className="navbar__item" onClick={() => setActive('Resume')}>Resume</div>
+                </Link> : null
+            }{
+                active !== 'Projects' &&
+                <Link to="projects">
+                    <div className="navbar__item" onClick={() => setActive('Projects')}>Projects</div>
+                </Link>
             }
             </div>
         </div>
