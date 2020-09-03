f2084481a4421ae849e6e4b13f6c2b07c3fe7e70
Никита Пермяков <permikov134@mail.ru>
2020-09-03 22:39:51 +0500

    correct style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import facebook from '../assets/icons/facebook.svg'
 import youtube from '../assets/icons/youtube.svg'
+import linkedin from '../assets/icons/linkedin.svg'
 import github from '../assets/icons/github.svg'
 import pin from '../assets/icons/pin.svg'
 import tie from '../assets/icons/tie.svg'
@@ -9,25 +10,28 @@ import avatar from '../assets/avatar.png'
 
 const Sidebar = () => {
     return (
-        <div>
+        <div className="sidebar">
             <img src={avatar} alt="avatar" className="sidebar__avatar" />
             <div className="sidebar__name">Permyakov <span>Nikita</span></div>
-            <div className="sidebar__item sidebar__title">Full-Stack (Python, JavaScript, C++)</div>
+            <div className="sidebar__item sidebar__title"><p>Full-Stack</p><p>Python, JavaScript, C++</p></div>
             <a href="https://drive.google.com/file/d/1HYt3qZudCBFhw5EhumidqXVtLDOwZobx/view">
-                <div className="sidebar__item">
+                <div className="sidebar__item sidebar__resume">
                     <img src={tie} alt="resume" className="sidebar__icon"/>Download Resume
                 </div>
             </a>
-            <figure className="sidebar__social-icons">
-                <a href="https://www.facebook.com/nikitapermikov/"><img src={facebook} alt="facebook" className="sidebar__icon"/></a>
-                <a href="https://www.youtube.com/channel/UCSdYMimys_AWyo015UK2gWA"><img src={youtube} alt="youtube" className="sidebar__icon"/></a>
+            <figure className="sidebar__social-icons my-2">
+                <a href="https://www.facebook.com/nikitapermikov/"><img src={facebook} alt="facebook" className="sidebar__icon mr-3"/></a>
+                <a href="https://www.youtube.com/channel/UCSdYMimys_AWyo015UK2gWA"><img src={youtube} alt="youtube" className="sidebar__icon mr-3"/></a>
+                <a href="https://www.linkedin.com/in/nikitapermikov/"><img src={linkedin} alt="linkedin" className="sidebar__icon"/></a>
             </figure>
-            <div className="contact">
-                <div className="sidebar__item">
-                    <a href="https://github.com/nikit34"><img src={github} alt="github" className="sidebar__icon"/>GitHub</a>
+            <div className="sidebar__contact">
+            <a href="https://github.com/nikit34">
+                <div className="sidebar__item sidebar__github">
+                    <img src={github} alt="github" className="sidebar__icon mr-3"/>GitHub
                 </div>
+            </a>
                 <div className="sidebar__location">
-                    <img src={pin} alt="location" className="sidebar__icon"/>
+                    <img src={pin} alt="location" className="sidebar__icon mr-3"/>
                     Russia, Moscow
                 </div>
                 <div className="sidebar__item">permikov134@yandex.ru</div>
