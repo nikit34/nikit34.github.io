2890a7432bd5e26573cc2ccd874b7677efe2ae5a
Никита Пермяков <permikov134@mail.ru>
2020-09-06 09:44:51 +0500

    enabled youtybe widget

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -2,12 +2,12 @@ import React from 'react'
 import github from '../assets/icons/github.svg'
 
 
-const ProjectCard = ({ projects: { name, image, deployed_url, github_url } }) => {
+const ProjectCard = ({ projects: { name, id_video, deployed_url, github_url } }) => {
     return (
         <div className="projectCard col-md-6 col-lg-4 my-2">
             <figure className="projectCard__wrapper">
                 <a href={deployed_url} target="_blank">
-                    <img src={image} alt={name} className="projectCard__image" />
+                <iframe id="ytplayer" alt={name} className="projectCard__video" type="text/html" src={`https://www.youtube.com/embed/${id_video}`} frameBorder="0" allowFullScreen></iframe>
                 </a>
                 <div className="projectCard__title">
                     <a href={github_url} target="_blank" rel="noopener noreferrer">
--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,18 +1,7 @@
-import tweetme from '../../assets/projects_images/tweetme.PNG'
-import chatapp from '../../assets/projects_images/chatapp.JPG'
-import hardware_store from '../../assets/projects_images/hardware_store.jpg'
-import portfolio from '../../assets/projects_images/portfolio.JPG'
-import new_year from '../../assets/projects_images/new_year.jpg'
-import color_classification from '../../assets/projects_images/color_classification.jpg'
-import blinking from '../../assets/projects_images/blinking.JPG'
-import COVID from '../../assets/projects_images/COVID.jpg'
-import Dev_talks from '../../assets/projects_images/Dev_talks.jpg'
-
-
 const data_projects = [
     {
         name: 'COVID 19 tracker',
-        image: COVID,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'https://covid-19-tracker-by-sumit.web.app/',
         github_url: 'https://github.com/Dey-Sumit/covid-19-tracker',
         category: ['react.js']
@@ -20,7 +9,7 @@ const data_projects = [
 
     {
         name: 'Dev Talks',
-        image: Dev_talks,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'https://dev-talks.herokuapp.com/',
         github_url: 'https://github.com/Dey-Sumit/Dev-talks',
         category: ['node.js', 'OpenCV', 'react.js']
@@ -28,7 +17,7 @@ const data_projects = [
 
     {
         name: 'Realtime Chat App',
-        image: chatapp,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'https://sumit-chat.netlify.app/',
         github_url: 'https://github.com/Dey-Sumit/chat-app-socket.io-react-node',
         category: ['node', 'Django', 'react']
@@ -36,7 +25,7 @@ const data_projects = [
 
     {
         name: 'Tweeter Clone',
-        image: tweetme,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'http://sumaxtweetme.pythonanywhere.com/',
         github_url: 'https://github.com/Dey-Sumit/tweetme',
         category: ['django', 'react']
@@ -44,28 +33,28 @@ const data_projects = [
 
     {
         name: 'Shop Website',
-        image: hardware_store,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: '!#',
         category: ['Django', 'Python']
     },
 
     {
         name: 'Dev Portfolio',
-        image: portfolio,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'http://suprateem.herokuapp.com/',
         category: ['Python']
     },
 
     {
         name: 'Bengali New Year',
-        image: new_year,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'https://dey-sumit.github.io/NewYear/',
         category: ['OpenCV']
     },
 
     {
         name: 'Color Classification using tf.js',
-        image: color_classification,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: '!#',
         github_url: 'https://github.com/Dey-Sumit/color-classification',
         category: ['Django', 'machine_learning']
@@ -73,14 +62,14 @@ const data_projects = [
 
     {
         name: 'Dev Port',
-        image: portfolio,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'http://suprateem.herokuapp.com/',
         category: ['Python']
     },
 
     {
         name: 'Blinking bubbles',
-        image: blinking,
+        id_video: 'M7lc1UVf-VE',
         deployed_url: 'https://dey-sumit.github.io/Blinking_Bubbles/',
         github_url: 'https://github.com/Dey-Sumit/Blinking_Bubbles',
         category: ['OpenCV']
