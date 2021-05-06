409fdfce89fd81371cb7cad6421cb219ce0393c8
nikit34 <permikov134@mail.ru>
2021-05-06 09:08:14 +0300

    fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -23,7 +23,7 @@ const Sidebar = function() {
         visible: {
             x: 0,
             transition: {
-                delay: 0.2, duration: 0.7, type: 'spring'
+                duration: 0.5, type: 'spring'
             }
         }
     }
