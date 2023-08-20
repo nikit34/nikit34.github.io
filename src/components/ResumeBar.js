4c62d65a4c1ae62f588f92e0d519cb73d553902a
nikit34 <permikov134@mail.ru>
2023-08-20 15:38:02 +0300

    Add years to experience

--- src/components/ResumeBar.js
+++ src/components/ResumeBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 
+const GENERAL_EXPERIENCE = 5
+
 const ResumeBar = function({ value: { icon, name, level } }) {
     const bar_width = `${level}%`
+    const years = GENERAL_EXPERIENCE * level / 100
     return (
         <div className="ResumeBar">
             <div className="bar__wrapper" style={{ 'width': bar_width }}>
                 <span className="bar__name">
                     <img src={icon} alt="icon" className="bar__icon mr-2" />
-                    {name}
+                    {name}, {years} years
                 </span>
             </div>
         </div>
