2ddc80bf543b49d8153d6584c0366466989c4001
nikit34 <permikov134@mail.ru>
2023-09-01 21:02:36 +0300

    Separate navbar styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import './navbar.css'
 
 
 const Navbar = function() {
