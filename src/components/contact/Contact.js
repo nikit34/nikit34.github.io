c274b0b4aaac0919e713719cd1dda8eaa6300958
nikit34 <permikov134@mail.ru>
2023-09-01 21:03:54 +0300

    Separate contact styles

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 
 import AboutSkillcard from '../about/AboutSkillcard'
 import { skills } from '../../data/about'
+import './contact.css'
 
 
 const Contact = function() {
