045b3490ef2672fc6dcffb297d82568b2f14b6f9
nikit34 <permikov134@mail.ru>
2023-09-01 23:27:06 +0300

    Remove left to contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-import AboutSkillcard from '../about/AboutSkillcard'
-import { skills } from '../../data/about'
 import './contact.css'
 
 
 const Contact = function() {
-    const about_variants = {
+    const contact_variants = {
         hidden: {
             opacity: 0
         },
@@ -26,24 +24,15 @@ const Contact = function() {
     }
 
     return (
-        <motion.div className="about"
-            variants={about_variants}
+        <motion.div className="contact"
+            variants={contact_variants}
             initial='hidden'
             animate='visible'
             exit='exit'
         >
-            <h6 className="about__intro">
-                I want to be professional in development.
-            </h6>
-            <div className="contaimer about__container">
-                <div className="row">
-                    {
-                        skills.map((skill, index) =>
-                            <AboutSkillcard skill={skill} key={index} />
-                        )
-                    }
-                </div>
-            </div>
+            <p className="contact__card-title">
+                test
+            </p>
         </motion.div>
     )
 }
