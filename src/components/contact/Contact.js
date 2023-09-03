dc8af8d796f46542cd6794d99049ef75ace25970
nikit34 <permikov134@mail.ru>
2023-09-03 15:34:12 +0300

    Add contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -59,102 +59,93 @@ const Contact = function() {
             animate='visible'
             exit='exit'
         >
-            <section className="contact section" id="contact">
-                <h2 className="section__title text-cs">Contact Me</h2>
-                <p className="section__subtitle">
-                    Let`s` <span>Talk About Ideas</span>
-                </p>
-                <div className="contact__container container grid">
-                    <div className="contact__content">
-                        <div className="contact__card">
-                            <span className="contact__card-icon">
-                                <FaRegMap />
-                            </span>
-                            <h3 className="contact__card-title">Address</h3>
-                            <p className="contact__card-data">North Tower, Toronto, Canada</p>
-                        </div>
-                        <div className="contact__card">
-                            <span className="contact__card-icon">
-                                <FaRegUser />
-                            </span>
-                            <h3 className="contact__card-title">Freelance</h3>
-                            <p className="contact__card-data">Available Right Now</p>
-                        </div>
-                        <div className="contact__card">
-                            <span className="contact__card-icon">
-                                <FaRegEnvelope />
-                            </span>
-                            <h3 className="contact__card-title">Email</h3>
-                            <p className="contact__card-data">permikov134@yandex.ru</p>
-                        </div>
-                        <div className="contact__card">
-                            <span className="contact__card-icon">
-                                <FaRegAddressBook />
-                            </span>
-                            <h3 className="contact__card-title">Phone</h3>
-                            <p className="contact__card-data">+7 977 991 80 74</p>
-                        </div>
-                    </div>
-                    <form className="contact__form" onSubmit={handleSubmit}>
-                        <div className="contact__form-group grid">
-                            <div className="contact__form-div">
-                                <label className="contact__form-tag text-cs">
-                                    Your full Name <b>*</b>
-                                </label>
-                                <input
-                                    type="text"
-                                    name="name"
-                                    onChange={handleChange}
-                                    value={form.name}
-                                    className="contact__form-input"
-                                />
-                            </div>
-                            <div className="contact__form-div">
-                                <label className="contact__form-tag text-cs">
-                                    Your Email Address <b>*</b>
-                                </label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    onChange={handleChange}
-                                    value={form.email}
-                                    className="contact__form-input"
-                                />
-                            </div>
+            <div className="contact__container container grid">
+                <form className="contact__form" onSubmit={handleSubmit}>
+                    <div className="contact__form-group grid">
+                        <div className="contact__form-div">
+                            <label className="contact__form-tag text-cs">
+                                Your full Name <b>*</b>
+                            </label>
+                            <input
+                                type="text"
+                                name="name"
+                                onChange={handleChange}
+                                value={form.name}
+                                className="contact__form-input"
+                            />
                         </div>
                         <div className="contact__form-div">
                             <label className="contact__form-tag text-cs">
-                                Your Subject <b>*</b>
+                                Your Email Address <b>*</b>
                             </label>
-                            <textarea
-                                name="subject"
+                            <input
+                                type="email"
+                                name="email"
                                 onChange={handleChange}
-                                value={form.subject}
-                                className='contact__form-input'
+                                value={form.email}
+                                className="contact__form-input"
                             />
                         </div>
-                        <div className="contact__form-div contact__form-area">
-                            <label className="contact__form-tag text-cs">
-                                Your Message <b>*</b>
-                            </label>
-                            <textarea
-                                name="message"
-                                onChange={handleChange}
-                                value={form.message}
-                                className='contact__form-input'
-                            />
-                        </div>
-                        <div className="contact__submit">
-                            <button type="submit" className="btn text-cs">
-                                Send Message
-                            </button>
-                        </div>
-                    </form>
+                    </div>
+                    <div className="contact__form-div">
+                        <label className="contact__form-tag text-cs">
+                            Your Subject <b>*</b>
+                        </label>
+                        <textarea
+                            name="subject"
+                            onChange={handleChange}
+                            value={form.subject}
+                            className='contact__form-input'
+                        />
+                    </div>
+                    <div className="contact__form-div contact__form-area">
+                        <label className="contact__form-tag text-cs">
+                            Your Message <b>*</b>
+                        </label>
+                        <textarea
+                            name="message"
+                            onChange={handleChange}
+                            value={form.message}
+                            className='contact__form-input'
+                        />
+                    </div>
+                    <div className="contact__submit">
+                        <button type="submit" className="btn text-cs">
+                            Send Message
+                        </button>
+                    </div>
+                </form>
+                <div className="contact__content grid">
+                    <div className="contact__card">
+                        <span className="contact__card-icon">
+                            <FaRegMap />
+                        </span>
+                        <h3 className="contact__card-title">Address</h3>
+                        <p className="contact__card-data">North Tower, Toronto, Canada</p>
+                    </div>
+                    <div className="contact__card">
+                        <span className="contact__card-icon">
+                            <FaRegUser />
+                        </span>
+                        <h3 className="contact__card-title">Freelance</h3>
+                        <p className="contact__card-data">Available Right Now</p>
+                    </div>
+                    <div className="contact__card">
+                        <span className="contact__card-icon">
+                            <FaRegEnvelope />
+                        </span>
+                        <h3 className="contact__card-title">Email</h3>
+                        <p className="contact__card-data">permikov134@yandex.ru</p>
+                    </div>
+                    <div className="contact__card">
+                        <span className="contact__card-icon">
+                            <FaRegAddressBook />
+                        </span>
+                        <h3 className="contact__card-title">Phone</h3>
+                        <p className="contact__card-data">+7 977 991 80 74</p>
+                    </div>
                 </div>
-                <div className="section__bg-wrapper">
-                    <span className="bg__title">Contact Me</span>
-                </div>
-            </section>
+            </div>
         </motion.div>
     )
 }
