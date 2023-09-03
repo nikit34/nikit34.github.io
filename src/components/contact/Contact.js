dd46c5e8de650f40127b891692d084ce6819ceba
nikit34 <permikov134@mail.ru>
2023-09-03 11:34:04 +0300

    Add send data to google sheets

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
+import { FaRegAddressBook, FaRegEnvelope, FaRegMap, FaRegUser } from 'react-icons/fa';
 
 import './contact.css'
 
@@ -23,6 +24,34 @@ const Contact = function() {
         }
     }
 
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setForm({...form, [name]: value});
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        fetch(
+            'https://script.google.com/macros/s/AKfycbyPtJyU5gNNyu9PdVj4NIW7MbVJDIa-6WX3CKa3Gaw9PtUdDtMJSYVadDI4L3LdZLaTUA/exec?name='
+            + form.name + '&email=' + form.email + '&subject=' + form.subject + '&message=' + form.message,
+            {
+                method: 'GET',
+                mode: 'no-cors'
+            }
+        ).then(() => {
+            setForm({name: '', email: '', subject: '', message: ''});
+        }).catch((error) => console.log(error));
+        console.log('Data sent successfully!');
+    };
+
     return (
         <motion.div className="contact"
             variants={contact_variants}
@@ -30,9 +59,97 @@ const Contact = function() {
             animate='visible'
             exit='exit'
         >
-            <p className="contact__card-title">
-                test
-            </p>
+            <section className="contact section" id="contact">
+                <h2 className="section__title text-cs">Contact Me</h2>
+                <p className="section__subtitle">
+                    Let`s` <span>Talk About Ideas</span>
+                </p>
+                <div className="contact__container container grid">
+                    <div className="contact__content">
+                        <div className="contact__card">
+                            <span className="contact__card-icon">
+                                <FaRegMap />
+                            </span>
+                            <h3 className="contact__card-title">Address</h3>
+                            <p className="contact__card-data">North Tower, Toronto, Canada</p>
+                        </div>
+                        <div className="contact__card">
+                            <span className="contact__card-icon">
+                                <FaRegUser />
+                            </span>
+                            <h3 className="contact__card-title">Freelance</h3>
+                            <p className="contact__card-data">Available Right Now</p>
+                        </div>
+                        <div className="contact__card">
+                            <span className="contact__card-icon">
+                                <FaRegEnvelope />
+                            </span>
+                            <h3 className="contact__card-title">Email</h3>
+                            <p className="contact__card-data">permikov134@yandex.ru</p>
+                        </div>
+                        <div className="contact__card">
+                            <span className="contact__card-icon">
+                                <FaRegAddressBook />
+                            </span>
+                            <h3 className="contact__card-title">Phone</h3>
+                            <p className="contact__card-data">+7 977 991 80 74</p>
+                        </div>
+                    </div>
+                    <form className="contact__form" onSubmit={handleSubmit}>
+                        <div className="contact__form-group grid">
+                            <div className="contact__form-div">
+                                <label className="contact__form-tag text-cs">
+                                    Your full Name <b>*</b>
+                                </label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    onChange={handleChange}
+                                    value={form.name}
+                                    className="contact__form-input"
+                                />
+                            </div>
+                            <div className="contact__form-div">
+                                <label className="contact__form-tag text-cs">
+                                    Your Email Address <b>*</b>
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                    value={form.email}
+                                    className="contact__form-input"
+                                />
+                            </div>
+                        </div>
+                        <div className="contact__form-div">
+                            <label className="contact__form-tag text-cs">
+                                Your Subject <b>*</b>
+                            </label>
+                            <input type="text" className="contact__form-input" />
+                        </div>
+                        <div className="contact__form-div contact__form-area">
+                            <label className="contact__form-tag text-cs">
+                                Your Message <b>*</b>
+                            </label>
+                            <textarea
+                                name="message"
+                                onChange={handleChange}
+                                value={form.message}
+                                className='contact__form-input'
+                            />
+                        </div>
+                        <div className="contact__submit">
+                            <button type="submit" className="btn text-cs">
+                                Send Message
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div className="section__bg-wrapper">
+                    <span className="bg__title">Contact Me</span>
+                </div>
+            </section>
         </motion.div>
     )
 }
