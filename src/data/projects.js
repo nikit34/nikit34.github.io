c4b8e1956b6a7eb42622602d1f57e82dbac49208
nikit34 <permikov134@mail.ru>
2023-12-21 00:28:37 +0000

    Add video

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,11 @@
 const data_projects = [
     {
+        name: 'Markov Chain',
+        id_video: 'kXpvcNE_EfQ',
+        github_url: 'https://github.com/nikit34/markov_chain_image_generation',
+        category: ['Python']
+    },
+    {
         name: 'Portfolio',
         id_video: '16Rlj9pGhJU',
         deployed_url: 'https://nikit34.github.io/nastya',
@@ -36,7 +42,6 @@ const data_projects = [
         github_url: 'https://github.com/nikit34/NodeNetwork_game_sfml',
         category: ['C++']
     },
-
     {
         name: 'Space Shooter: setup ➡ play!',
         id_video: 'hCFJztzs2vY',
@@ -44,7 +49,6 @@ const data_projects = [
         github_url: 'https://github.com/nikit34/Space_shooter',
         category: ['C++']
     },
-
     {
         name: 'Online Store',
         id_video: '7Yrl9mgb7Bs',
@@ -52,7 +56,6 @@ const data_projects = [
         github_url: 'https://github.com/nikit34/eCommerce_Django',
         category: ['Django', 'Python', 'JavaScript']
     },
-
     {
         name: 'Map Market Place',
         id_video: 'bQ9iaNWJ404',
@@ -60,7 +63,6 @@ const data_projects = [
         github_url: 'https://github.com/nikit34/mapMarketPlace',
         category: ['Django', 'Python', 'JavaScript']
     },
-
     {
         name: 'Previous Portfolio v2',
         id_video: 'ErIs2gI8yk0',
@@ -68,7 +70,6 @@ const data_projects = [
         github_url: 'https://github.com/nikit34/nikit34.github.io_v2',
         category: ['JavaScript', 'React']
     },
-
     {
         name: 'Tools for Graph theory',
         id_video: '9oUhFav3YIE',
@@ -76,14 +77,12 @@ const data_projects = [
         github_url: 'https://github.com/nikit34/tool_for_calc_graph',
         category: ['JavaScript', 'Node']
     },
-
     {
         name: 'Digital Education',
         id_video: 'XhD2iMbgBlE',
         github_url: 'https://github.com/nikit34/Django_HackMoscow',
         category: ['Django', 'Python']
     },
-
     {
         name: 'Accounting traffic flow from surveillance cameras',
         id_video: 'V-YdzegvIOM',
