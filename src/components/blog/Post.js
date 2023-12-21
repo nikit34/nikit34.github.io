7322350fe1385979de7790e3209f297d8bb78213
nikit34 <permikov134@mail.ru>
2023-12-21 02:23:32 +0000

    Add motion

--- src/components/blog/Post.js
+++ src/components/blog/Post.js
@@ -4,23 +4,33 @@ import { motion } from 'framer-motion'
 
 const Post = function({ post: { title, link, img } }) {
     return (
-        <div className="col-lg-6">
-            <div className="skill-card">
-                <div className="row">
-                    <div className="col-2">
-                        <img src={icon} alt="icon" className="skill-card__icon" />
+        <motion.div
+            layout
+            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 1, scale: 0.9 }}
+            exit={{ opacity: 1, scale: 0.9 }}
+            transition={{ duration: 0.2 }}
+            className="blog-post"
+            key={title}
+        >
+            <div className="col-lg-6">
+                <div className="skill-card">
+                    <div className="row">
+                        <div className="col-2">
+                            <img src={icon} alt="icon" className="skill-card__icon" />
+                        </div>
+                        <div className="col-10">
+                            <h6 className="skill-card__title">{title}</h6>
+                        </div>
                     </div>
-                    <div className="col-10">
-                        <h6 className="skill-card__title">{title}</h6>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <p className="skill-card__content">{about}</p>
+                    <div className="row">
+                        <div className="col">
+                            <p className="skill-card__content">{about}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
