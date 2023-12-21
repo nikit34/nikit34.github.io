5691f9dc78c4d3ed1b419677561271b30b660e0a
nikit34 <permikov134@mail.ru>
2023-12-21 02:19:18 +0000

    Refactoring

--- src/components/projects/Card.js
+++ src/components/projects/Card.js
@@ -16,30 +16,30 @@ const Card = function({ project: { name, id_video, deployed_url, github_url } })
             className="projectCard col-md-6 col-lg-4"
             key={name}
         >
-            <figure className="projectCard__wrapper">
-                <iframe id="ytplayer" alt={name} className="projectCard__video" type="text/html" src={`https://www.youtube.com/embed/${id_video}`} frameBorder="0" allowFullScreen></iframe>
-                <div className="projectCard__context">
+            <figure className="project-card__wrapper">
+                <iframe id="ytplayer" alt={name} className="project-card__video" type="text/html" src={`https://www.youtube.com/embed/${id_video}`} frameBorder="0" allowFullScreen></iframe>
+                <div className="project-card__context">
                     <div className="row justify-content-between">
                         {
                             !!(github_url) ?
                                 <div className="col">
                                     <a href={github_url} target="_blank" rel="noopener noreferrer">
-                                        <img src={github} alt="github link" className="projectCard__icon" /> Code
+                                        <img src={github} alt="github link" className="project-card__icon" /> Code
                                     </a>
                                 </div>
-                            : <div className="col"><img src={secretly} alt="secretly link" className="projectCard__icon" />Secretly</div>
+                            : <div className="col"><img src={secretly} alt="secretly link" className="project-card__icon" />Secretly</div>
                         }
                         {
                             !!(deployed_url) ?
                                 <div className="col text-right">
                                     <a href={deployed_url} target="_blank" rel="noopener noreferrer">
-                                        <img src={joystick} alt="joystick link" className="projectCard__icon" />Demo
+                                        <img src={joystick} alt="joystick link" className="project-card__icon" />Demo
                                     </a>
                                 </div>
-                            : <div className="col"><img src={secretly} alt="secretly link" className="projectCard__icon" />Discontinued</div>
+                            : <div className="col"><img src={secretly} alt="secretly link" className="project-card__icon" />Discontinued</div>
                         }
                     </div>
-                    <div className="row projectCard__description">
+                    <div className="row project-card__description">
                         <div className="col">
                             {name}
                         </div>
